60be5f8dc753be7b8cf835ee21ee8898a8f383bf
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-05-08 01:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const changeBtn = document.getElementById("change_button");
+    const resetBtn = document.getElementById("Reset");
+    const gridItems = document.querySelectorAll("#grid-item");
+
+    // Change Color
+    changeBtn.addEventListener("click", () => {
+
+      const blockId = document.getElementById("block_id").value;
+      const color = document.getElementById("colour_id").value;
+
+      // Reset all colors first
+      gridItems.forEach((item) => {
+        item.style.backgroundColor = "transparent";
+      });
+
+      // Change selected block color
+      if(blockId >= 1 && blockId <= 9){
+        gridItems[blockId - 1].style.backgroundColor = color;
+      }
+    });
+
+    // Reset Button
+    resetBtn.addEventListener("click", () => {
+      gridItems.forEach((item) => {
+        item.style.backgroundColor = "transparent";
+      });
+    });
